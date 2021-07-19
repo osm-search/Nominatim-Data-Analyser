373af05a915f6ef46a08a37af61cfe5f6e058ce8
AntoJvlt <antonin.jolivat@gmail.com>
2021-07-19 23:55:30 +0200

    Send geojson features to the stdin of tippecanoe. Updates rules specification to remove call to geojsonformatter when using vector tiles converter

--- analyser/core/pipes/output_formatters/vector_tiles/vector_tile_converter.py
+++ analyser/core/pipes/output_formatters/vector_tiles/vector_tile_converter.py
@@ -1,11 +1,14 @@
 from __future__ import annotations
+from geojson.feature import Feature, FeatureCollection
+from geojson import dumps
 from analyser.logger.logger import LOG
 from analyser.logger.timer import Timer
 from analyser.core.model import Paths
 from analyser.core import Pipe
 from pathlib import Path
-import os
+from typing import List
 import typing
+import subprocess
 
 if typing.TYPE_CHECKING:
     from analyser.core.qa_rule import ExecutionContext
@@ -21,34 +24,34 @@ class VectorTileConverter(Pipe):
         self.base_folder_path = Path('/srv/nominatim/data-files/vector-tiles')
         self.folder_name = folder_name
 
-    def process(self, geojson_paths: Paths) -> Paths:
+    def process(self, features: List[Feature]) -> Paths:
         """
             Converts a GeoJSON file to Vector tiles by
             calling tippecanoe from the command line.
         """
+        feature_collection = FeatureCollection(features)
         output_dir = Path(self.base_folder_path / Path(self.folder_name))
         output_dir.mkdir(parents=True, exist_ok=True)
         timer = Timer().start_timer()
 
-        os.system(f"""
-            tippecanoe --output-to-directory {output_dir} \
-            --no-tile-compression \
-            --generate-ids \
-            --layer=layer \
-            --name="New Layer" \
-            --description="Description of new layer" \
-            --attribution='Copyright OpenStreetMap contributors' \
-            --base-zoom=6 \
-            --maximum-tile-bytes=50000 \
-            --drop-densest-as-needed \
-            --quiet \
-            --force \
-            --no-clipping \
-            {geojson_paths.local_path}
-        """)
-        
-        #Remove the geojson file as it is only used for conversion.
-        Path(geojson_paths.local_path).unlink()
+        try:
+            result = subprocess.run(
+                ['tippecanoe', f'--output-to-directory={output_dir}', 
+                '--force',
+                '--no-tile-compression',
+                '-zg',
+                '-K 60',
+                '-r1',
+                '--drop-densest-as-needed'],
+                check=True,
+                input=dumps(feature_collection).encode(),
+                stdout=subprocess.PIPE
+            )
+            LOG.info(result)
+        except subprocess.TimeoutExpired as e:
+            LOG.fatal(e)
+        except subprocess.CalledProcessError as e:
+            LOG.fatal(e)
 
         LOG.info('Vector tile conversion executed in %s mins %s secs', *timer.get_elapsed())
         web_path = FULL_PATH_PREFIX + '/' + self.folder_name + '/{z}/{x}/{y}.pbf'
