07b415b797ac47d1061c3c4f0fbae25c2f9b160a
Sarah Hoffmann <lonvia@denofr.de>
2024-09-27 09:54:47 +0200

    do not hard-code rule dir in yaml loader
    Use rule dir from Core object instead.

--- src/nominatim_data_analyser/core/core.py
+++ src/nominatim_data_analyser/core/core.py
@@ -9,7 +9,7 @@ class Core():
     """
         Core of the analyser used to execute rules.
     """
-    def __init__(self, config_file: str | None) -> None:
+    def __init__(self, config_file: str | Path | None) -> None:
         load_config(None if config_file is None else Path(config_file))
         self.rules_path = Path(__file__, '..', '..', 'rules_specifications').resolve()
 
@@ -22,13 +22,16 @@ class Core():
         """
         for rule_file in self.rules_path.glob('*.yaml'):
             if not filter or rule_file.stem not in filter:
-                self.execute_one(rule_file.stem)
+                self._execute(rule_file)
 
     def execute_one(self, name: str) -> None:
         """
             Execute one QA rule based on its YAML file name.
         """
+        self._execute(self.rules_path / f"{name}.yaml")
+
+    def _execute(self, rule_file: Path) -> None:
         timer = Timer().start_timer()
-        loaded_yaml = load_yaml_rule(name)
-        PipelineAssembler(loaded_yaml, name).assemble().process_and_next()
-        LOG.info('<%s> The whole rule executed in %s mins %s secs', name, *timer.get_elapsed())
+        loaded_yaml = load_yaml_rule(rule_file)
+        PipelineAssembler(loaded_yaml, rule_file.stem).assemble().process_and_next()
+        LOG.info('<%s> The whole rule executed in %s mins %s secs', rule_file.stem, *timer.get_elapsed())
